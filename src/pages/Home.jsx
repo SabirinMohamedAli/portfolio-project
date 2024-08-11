2f2aff4c7b0fa1b782d14d62a1d0f727a83e2322
SabirinMohamedAli <thaprinmohamett1333@gmail.com>
2024-08-11 19:12:19 +0300

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import heroImage from '../images/3.jpg'; 
+import heroImage from '../images/4.png'; 
 
 function Home() {
   return (
-    <div className="flex flex-col-reverse md:flex-row items-center justify-center h-screen bg-white text-dark-blue  mt-[-10px]">
-      <div className="md:w-1/2 p-8 md:p-16">
-        <h2 className="text-5xl font-bold mb-4" style={{ fontFamily: 'cursive', fontSize:'-30px', marginBottom: '10px' }}>
+    <div className="flex flex-col-reverse md:flex-row items-center justify-center h-screen bg-white text-dark-blue mt-[1px]">
+      <div className="md:w-1/2 p-8 md:p-12">
+        <h2 className="text-5xl font-bold mb-2" style={{ fontFamily: 'cursive', marginBottom: '10px' }}>
           Hi, I am Adnan
         </h2>
-        <p className="text-3xl mb-6" style={{ fontFamily: 'cursive', color: '#FF5733' }}>
+        <p className="text-3xl mb-4" style={{ fontFamily: 'cursive', color: '#FF5733' }}>
           Full Stack Developer
         </p>
-        <p className="text-3xl mb-6" style={{ fontFamily: 'cursive', color: '#34495E' }}>
-          Network Engneering
+        <p className="text-3xl mb-4" style={{ fontFamily: 'cursive', color: '#34495E' }}>
+          Network Engineering
         </p>
-
 
         <p className="text-xl mb-4">Building efficient and scalable web applications.</p>
         <p className="text-xl mb-4"  style={{ fontFamily: 'cursive', color: '#FF5733', fontWeight: 'bold', fontSize: '20px' }}>I specialize in Node.js, React, Java, JavaScript and C#.</p>
@@ -28,8 +27,8 @@ function Home() {
           </button>
         </div>
       </div>
-      <div className="md:w-1/2 flex justify-center">
-        <img src={heroImage} alt="Hero" className="w-1/2 h-auto object-contain" style={{ backgroundColor: 'transparent' }} />
+      <div className="md:w-0.5/1 flex justify-center">
+        <img src={heroImage} alt="Hero" className="w-2/2 h-auto object-contain" style={{ backgroundColor: 'transparent' }} />
       </div>
     </div>
   );
